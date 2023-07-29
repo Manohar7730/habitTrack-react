e386e97c6a2748a3864491c5cabdda33fa874a72
Manohar <manohar20century@gmail.com>
2023-07-29 11:47:51 +0530

    Completed Reducers for adding,updating status,deleting habits and store and imported data and store

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import { useSelector } from "react-redux";
+
 function Home() {
-  return <div>Home</div>;
+  const habits = useSelector((state) => state.habits.allHabits);
+  return (
+    <Container>
+      <h1>Habit Tracker</h1>
+      <Card>
+        <Card.Body>
+          <h2>Today Statuses:</h2>
+          <ul>
+            {habits.map((habit) => (
+              <li key={habit.id}>
+                <h3>{habit.title}</h3>
+              </li>
+            ))}
+          </ul>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default Home;
